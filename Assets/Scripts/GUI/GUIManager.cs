9763dc2d2174f85900cb007a9e52c1344b3a2501
JuanMarGam <99370854+JuanMarGam@users.noreply.github.com>
2024-02-03 15:19:33 -0500

    Updated Pause Menu

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -7,14 +7,50 @@ public class GUIManager : MonoBehaviour
 {
     [SerializeField] GameObject GUI;
     [SerializeField] GameObject pauseCanva;
-
-    public void displayPauseCanva()
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip clickSound;
+    [SerializeField] AudioClip hoverSound;
+    private bool isActivated;
+    private void Awake()
+    {
+        GUI.SetActive(true);
+        pauseCanva.SetActive(false);
+    }
+    private void Start()
+    {
+        isActivated = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&& isActivated== false)
+        {
+            
+            displayPauseCanvas();
+            isActivated = true;
+        }else if(Input.GetKeyDown(KeyCode.Escape) && isActivated == true)
+        {
+            closePauseCanvas();
+            isActivated = false;
+        }
+    }
+    
+    public void displayPauseCanvas()
     {
         GUI.SetActive(false);
         pauseCanva.SetActive(true);
     }
+    public void closePauseCanvas()
+    {
+        pauseCanva.SetActive(false);
+        GUI.SetActive(true);
+    }
     public void ReturnMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    public void ClickSound()
+    {
+        audioSource.clip = clickSound;
+        audioSource.Play();
     }
 }
