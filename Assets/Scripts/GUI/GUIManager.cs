48987e9054aca8a37e0d720f534a8a9fefa429b2
JuanMarGam <99370854+JuanMarGam@users.noreply.github.com>
2024-02-03 19:05:34 -0500

    Merged master, and implemented Main Menu, Level 1 , and game over flow. Also, the GUI is working on the level 1 scene.

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -7,6 +7,7 @@ public class GUIManager : MonoBehaviour
 {
     [SerializeField] GameObject GUI;
     [SerializeField] GameObject pauseCanva;
+    [SerializeField] GameObject inventoryCanva;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip clickSound;
     [SerializeField] AudioClip hoverSound;
@@ -15,6 +16,7 @@ public class GUIManager : MonoBehaviour
     {
         GUI.SetActive(true);
         pauseCanva.SetActive(false);
+        inventoryCanva.SetActive(false);
     }
     private void Start()
     {
@@ -24,12 +26,14 @@ public class GUIManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape)&& isActivated== false)
         {
-            
+            PauseGame();
             displayPauseCanvas();
             isActivated = true;
         }else if(Input.GetKeyDown(KeyCode.Escape) && isActivated == true)
         {
             closePauseCanvas();
+            //closeInventoryCanvas();
+            ResumeGame();
             isActivated = false;
         }
     }
@@ -44,13 +48,32 @@ public class GUIManager : MonoBehaviour
         pauseCanva.SetActive(false);
         GUI.SetActive(true);
     }
+    public void closeInventoryCanvas()
+    {
+        inventoryCanva.SetActive(false);
+        pauseCanva.SetActive(true);
+    }
+    public void openInventoryCanvas()
+    {
+        pauseCanva.SetActive(false);
+        inventoryCanva.SetActive(true);
+    }
     public void ReturnMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
     }
     public void ClickSound()
     {
         audioSource.clip = clickSound;
         audioSource.Play();
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 }
